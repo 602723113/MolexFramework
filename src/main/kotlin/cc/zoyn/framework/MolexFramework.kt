1e3296066126c39d6ad40055e3a934f0081968b8
Zoyn <602723113@qq.com>
2018-02-26 12:50:41 +0800

    :tada:init: initial Project

---
+++ src/main/kotlin/cc/zoyn/framework/MolexFramework.kt
@@ -0,0 +1,16 @@
+package cc.zoyn.framework
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * Main Class
+ *
+ * @author Zoyn
+ * @since 2018-02-26
+ */
+class MolexFramework : JavaPlugin() {
+
+    override fun onEnable() {
+    }
+
+}
